78f0a5446afd7bf5022acab603b20f4e31146122
Zhiwei <zhiweixuchn@gmail.com>
2021-06-28 01:13:30 +0800

    still have w and v trainable bug

--- examples/ml-1m.py
+++ examples/ml-1m.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import pandas as pd
 import tensorflow as tf
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.optimizers import SGD
 
 from pyrec.models import FM
 from pyrec.utils import train_validation_test_split
@@ -31,13 +33,13 @@ def main():
     # hyper-parameter
     train_size, validation_size, test_size = [0.6, 0.2, 0.2]
     batch_size = 64
-    learning_rate = 1e-4
+    learning_rate = 1e-6
     epochs = 50
 
     # construct the dataset
     labels = df.pop('ctr')
     dataset = tf.data.Dataset.from_tensor_slices((dict(df), labels))
-    dataset = dataset.shuffle(buffer_size=200, seed=42).batch(64)
+    dataset = dataset.shuffle(buffer_size=200, seed=42).batch(batch_size)
     train_dataset, validation_dataset, test_dataset = train_validation_test_split(
         dataset, len(df), train_size, validation_size
     )
@@ -49,9 +51,17 @@ def main():
         dense_feature_columns,
         use_dense_feature_columns=False,
     )
-    for features, labels in train_dataset:
-        print(model(features))
-        break
+    print(model.trainable_variables)
+    loss = BinaryCrossentropy()
+    optimizer = SGD(learning_rate=learning_rate)
+    for epoch in range(epochs):
+        for features, labels in train_dataset:
+            with tf.GradientTape() as tape:
+                logits = model(features)
+                loss_value = loss(labels, logits)
+            grads = tape.gradient(loss_value, model.trainable_variables)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            print(loss_value.values)
 
 
 if __name__ == '__main__':
--- pyrec/layers/fm.py
+++ pyrec/layers/fm.py
@@ -9,6 +9,8 @@ class FM(tf.keras.layers.Layer):
     def __init__(self, k):
         super(FM, self).__init__()
         self.k = k
+        # self.w = tf.Variable(tf.random.normal(shape=(9992, 1)))
+        # self.v = tf.Variable(tf.random.normal(shape=(9992, self.k)))
         self.w = None
         self.v = None
 
@@ -20,10 +22,10 @@ class FM(tf.keras.layers.Layer):
         Returns:
             logits
         """
-        if not self.w and not self.v:
-            dimension = inputs.shape[1]
-            self.w = tf.Variable(tf.random.normal(shape=(dimension, 1)))
-            self.v = tf.Variable(tf.random.normal(shape=(dimension, self.k)))
+        # if not self.w and not self.v:
+        #     dimension = inputs.shape[1]
+        #     self.w = tf.Variable(tf.random.normal(shape=(dimension, 1)))
+        #     self.v = tf.Variable(tf.random.normal(shape=(dimension, self.k)))
 
         logits = tf.squeeze(tf.matmul(inputs, self.w))
         square_of_sum = tf.square(tf.matmul(inputs, self.v))
