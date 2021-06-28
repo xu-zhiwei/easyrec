ee1552efa7a3d049ba01a5f495cb76405510947e
Zhiwei <zhiweixuchn@gmail.com>
2021-06-28 08:32:16 +0800

    add a get shape

--- pyrec/utils.py
+++ pyrec/utils.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 
 import tensorflow as tf
 
@@ -8,8 +8,20 @@ def train_validation_test_split(dataset: tf.data.Dataset,
                                 train_size: float,
                                 validation_size: float
                                 ) -> Tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]:
+    if train_size + validation_size >= 1:
+        raise ValueError('train_size + validation_size should be less than 1')
     train_dataset = dataset.take(round(train_size * dataset_size))
     test_dataset = dataset.skip(round(train_size * dataset_size))
     validation_dataset = test_dataset.take(round(validation_size * dataset_size))
     test_dataset = test_dataset.skip(round(validation_size * dataset_size))
     return train_dataset, validation_dataset, test_dataset
+
+
+def get_input_dimension(one_hot_feature_columns,
+                        multi_hot_feature_columns,
+                        dense_feature_columns,
+                        inputs):
+    one_hot_input_layer = tf.keras.layers.DenseFeatures(feature_columns=one_hot_feature_columns)
+    multi_hot_input_layer = tf.keras.layers.DenseFeatures(feature_columns=multi_hot_feature_columns)
+    dense_feature_input_layer = tf.keras.layers.DenseFeatures(feature_columns=dense_feature_columns)
+
