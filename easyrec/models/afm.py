7757fc09c91699cb7cebade1ba18dd8f75df3c5f
Zhiwei Xu <zhiweixuchn@gmail.com>
2021-08-18 17:13:34 +0800

    Fix autodoc bugs

--- easyrec/models/afm.py
+++ easyrec/models/afm.py
@@ -9,15 +9,17 @@ class AFM(tf.keras.models.Model):
     Attentional Factorization Machines (AFM).
     Reference: Jun Xiao et al. Attentional Factorization Machines:Learning the Weight of Feature Interactions
     via Attention Networks. arXiv. 2017.
-
-    Args:
-        one_hot_feature_columns: List[CategoricalColumn] encodes one hot feature fields, such as sex_id.
-        k: Dimension of the second-order weights.
     """
 
     def __init__(self,
                  one_hot_feature_columns,
                  k=16):
+        """
+
+        Args:
+            one_hot_feature_columns: List[CategoricalColumn] encodes one hot feature fields, such as sex_id.
+            k: Dimension of the second-order weights.
+        """
         super(AFM, self).__init__()
         if not one_hot_feature_columns:
             raise ValueError('len(one_hot_feature_columns) should greater than 0')
